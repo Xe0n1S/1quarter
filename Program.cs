2096d4ee8244c7fb18862ca2e1c68a78e2b64398
Xe0n1S <master.5@mail.ru>
2022-11-27 21:59:37 +0400

    3

--- Program.cs
+++ Program.cs
@@ -1,4 +1,14 @@
-﻿void PrintArray(string[] array)
+﻿string[] array = new string[] {"Hello", "2", "world", ":-)"};
+int Array = 0;
+for (int i = 0; i <= array.Length - 1; i++)
+{
+    if (array[i].Length <= 3) Array++;
+}
+
+PrintArray(array);
+
+
+void PrintArray(string[] array)
 {
     for (int i = 0; i < array.Length; i++)
     {
